f4727bd29b17274094be68c637e0e6abf4ba97ad
unknown <newjincar33@gmail.com>
2021-01-28 16:19:20 +0900

    버그 수정

--- auto-eduro.py
+++ auto-eduro.py
@@ -7,28 +7,6 @@ from time import sleep
 
 class Macro:
     def __init__(self):
-        def load_userdata():
-            try:
-                file = open('eduro_userdata', 'r')
-            except FileNotFoundError:
-                driver = webdriver.Chrome(executable_path=chromedriver())
-                driver.get('https://hcs.eduro.go.kr/')
-                while True:
-                    self.users = driver.execute_script('''return localStorage.getItem('users');''')
-                    self.password = driver.execute_script('''return document.querySelector('input[type="password"]')?.value;''')
-                    if self.users:
-                        file = open('eduro_userdata', 'w')
-                        file.write('\n'.join([self.users, self.password]))
-                        file.close()
-                        break
-                    sleep(0.1)
-                driver.close()
-            else:
-                self.users, self.password = file.read().split('\n')
-                file.close()
-
-        load_userdata()
-
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         options.add_argument('window-size=1920x1080')
@@ -38,10 +16,31 @@ class Macro:
 
         self.driver = webdriver.Chrome(executable_path=chromedriver(), options=options)
         self.driver.get('https://hcs.eduro.go.kr/')
-        self.driver.execute_script(f'''localStorage.setItem('users', '{self.users}');''')
-        self.driver.refresh()
 
         self.wait = WebDriverWait(self.driver, 10)
+
+    def load_userdata(self):
+        try:
+            file = open('eduro_userdata', 'r')
+        except FileNotFoundError:
+            temp_driver = webdriver.Chrome(executable_path=chromedriver())
+            temp_driver.get('https://hcs.eduro.go.kr/')
+            while True:
+                self.users = temp_driver.execute_script('''return localStorage.getItem('users');''')
+                self.password = temp_driver.execute_script('''return document.querySelector('input[type="password"]')?.value;''')
+                if self.users:
+                    file = open('eduro_userdata', 'w')
+                    file.write('\n'.join([self.users, self.password]))
+                    file.close()
+                    break
+                sleep(0.1)
+            temp_driver.close()
+        else:
+            self.users, self.password = file.read().split('\n')
+            file.close()
+
+        self.driver.execute_script(f'''localStorage.setItem('users', '{self.users}');''')
+        self.driver.refresh()
 
     def self_check(self):
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="password"]'))).send_keys(self.password)
@@ -55,8 +54,16 @@ class Macro:
             answer.click()
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="submit"]'))).click()
 
+    def update_userdata(self):
+        self.users = self.driver.execute_script('''return localStorage.getItem('users');''')
+        file = open('eduro_userdata', 'w')
+        file.write('\n'.join([self.users, self.password]))
+        file.close()
+
     def __del__(self):
         self.driver.close()
 
 macro = Macro()
+macro.load_userdata()
 macro.self_check()
+macro.update_userdata()
